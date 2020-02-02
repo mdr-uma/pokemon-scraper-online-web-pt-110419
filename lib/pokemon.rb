d219ab765f861d5a0aa70e44d5aaa1548e87ca25
Uma Manandhar <um_2003_8@hotmail.com>
2020-02-02 21:34:36 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,19 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+
+  def initialize(id:, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
+
+  def save
+    sql = <<-SQL
+      INSERT INTO pokemon (name, type, db)
+      VALUES (?, ?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.type, self.db)
+  end
 end
